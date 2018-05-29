d3613c44c7d7d077ad1a99757e4ab139cc271811
Matteo Alessio Carrara <sw.matteoac@gmail.com>
2018-05-29 16:19:19 +0200

    Cleaned & added library

--- mi.py
+++ mi.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Copyright 2017 Matteo Alessio Carrara <sw.matteoac@gmail.com>
+# Copyright 2018 Matteo Alessio Carrara <sw.matteoac@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,32 +15,111 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sqlite3
-from sys import stdout
-import time
+# This script is used to create csv data from "Mi Band Master" android app database
 
-def exp(sel, table):
-	of = open("0%s.csv" %(table), "w")
-	of.write(sel+"\n")
-	for row in c.execute("SELECT %s FROM %s" % (sel, table)):
-		for i in range(len(row)):
-			col = str(row[i])
-			out = col + ", " if i != (len(row) - 1) else col + "\n"
-			of.write(out)
-	of.close()
+import sqlite3
+import time
+import os
+from sys import stdout, stderr, argv
+
+
+class Table:
+	def _filename(self, table):
+		return table
+		
+	def _process(self, data):
+		return data
+		
+	def exp(self, sel, table):
+		of = open("%s/%s.csv" %(EXPDIR, self._filename(table)), "w")
+		c.execute("SELECT %s FROM %s" % (sel, table))
+		for row in self._process(c.fetchall()):
+			for i in range(len(row)):
+				col = str(row[i])
+				out = col + ", " if i != (len(row) - 1) else col + "\n"
+				of.write(out)
+		of.close()
+		
+class Sleep(Table):
+	def _filename(self, table):
+		return "sleepcurve"
+		
+	def _process(self, data):
+		newdata = []
+		for row in data:
+			newdata.append([row[0]+7200, 1])
+			newdata.append([row[1]+7200, 0])
+		return newdata
+		
+class Sleep2(Table):
+	def _filename(self, table):
+		return "sleeptime"
+		
+	def _process(self, data):
+		newdata = []
+		for row in data:
+			newdata.append(list(row)+[row[0]+row[1]])
+		return newdata
+		
+class Heart(Table):
+	def _process(self, data):
+		newdata = []
+
+		for row in data:
+			tmp = list(row) # time, hr
+			tmp[0] = tmp[0] + 7200 # fixing timezone
+			newdata.append(tmp)
+
+		return newdata
 	
 
-db = sqlite3.connect("miband")
-c = db.cursor()
+
+
+
 
 # CREATE TABLE `heartrate` (`time` integer primary key,`hr` integer);
-exp("time, hr", "heartrate")
+Heart().exp("time, hr", "heartrate")
 
 # CREATE TABLE `sleep` (`id` integer primary key,`start_time` integer,`end_time` 
 # integer,`awake` integer,`deep` integer,`light` integer,`stages` text,`deleted` 
 # integer,`manually` integer);
-exp("start_time, end_time, awake, deep, light, stages", "sleep")
+Sleep().exp("start_time, end_time", "sleep")
+Sleep2().exp("deep, light", "sleep")
 
 # CREATE TABLE `steps` (`time` integer primary key,`intensity` integer,`steps` 
 # integer,`category` integer);
-exp("time, intensity, steps, category", "steps")
+Table().exp("time, intensity, steps, category", "steps")
+
+
+def export_table(tname):
+	with open("%s/%s.csv" %(EXPDIR, tname), "w") as of:
+		pass
+
+
+
+dbname = argv[1]
+
+db = sqlite3.connect(dbname)
+c = db.cursor()
+
+EXPDIR = str(time.time()) + "_exp"
+os.mkdir(EXPDIR)
+
+# db schema last updated 2018/06/14
+
+
+
+# CREATE TABLE `heartrate` (`time` integer primary key,`hr` integer);
+	c.execute("SELECT time, hr FROM heartrate")
+	for row in self._process(c.fetchall()):
+
+
+		for i in range(len(row)):
+			col = str(row[i])
+			out = col + ", " if i != (len(row) - 1) else col + "\n"
+			of.write(out)
+
+
+
+
+
